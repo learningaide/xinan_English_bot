f7754be6f9c38b72b472afb4f0ecd69681bb9b48
jonathanlehner <lehnerj@student.ethz.ch>
2022-02-26 22:19:11 +0100

    forwarder works

---
+++ intergram/forwarder.js
@@ -0,0 +1,44 @@
+const WebSocket = require('ws');
+
+console.log("starting forwarder")
+const ws_AI = new WebSocket('ws://localhost:10001/websocket', {
+  perMessageDeflate: false
+});
+
+const ws_chat = new WebSocket('ws://localhost:8080/websocket', {
+  perMessageDeflate: false
+});
+
+ws_AI.on('open', function open() {
+    console.log("connection openend to AI server");
+});
+
+ws_chat.on('open', function open() {
+    console.log("connection openend to chat server");
+});
+
+ws_chat.on('message', function message(data) {
+    const parsedData = JSON.parse(data)
+    console.log('received: %s', parsedData);
+    ws_AI.send(data);
+    }
+);  
+
+ws_AI.on('open', () => {
+    console.log("connection openend to chat server");
+    ws_AI.send(JSON.stringify({"text":'hi'}));
+    ws_AI.send(JSON.stringify({"text":'begin'}));
+});
+
+counter = 0;
+ws_AI.on('message', function message(data) {
+    const parsedData = JSON.parse(data)
+    console.log('received: %s', parsedData);
+    console.log(counter)
+    if(counter >= 2){
+        ws_chat.send(data);
+    }
+    counter++;
+    }
+);  
+
--- intergram/server.js
+++ intergram/server.js
@@ -13,44 +13,56 @@ let global_user_id = 0;
 let global_chat_id = 0;
 const found_words = {}
 
-const ws = new WebSocket('ws://ai.real-impact.org:10001/websocket', {
+/*let ws = new WebSocket('ws://ai.real-impact.org:10001/websocket', {
   perMessageDeflate: false
-});
-ws.on('open', function open() {
-    ws.send(JSON.stringify({"text":'hi'}));
-    ws.send(JSON.stringify({"text":'begin'}));
-});
-ws.on('message', function message(data) {
-    const parsedData = JSON.parse(data)
-    console.log('received: %s', parsedData);
-    console.log(global_user_id);
-    console.log(global_chat_id);
-    const text = parsedData["text"];
-    console.log(text)
-    io.to(global_user_id).emit(global_chat_id + "-" + global_user_id, {from: "admin", text, name: "AI"});
-    const words = ["apple", "tree", "Eiffel", "Germany", "excellent"]
-    const chatId = global_chat_id;
-    if(words.some(el => text.includes(el))){
-        console.log("bingo word "+text)
-        const bingo_words = words.filter(el => text.includes(el));
-        console.log(bingo_words);
-        if(!found_words[chatId]){
-            found_words[chatId] = [];
-        }
-        found_words[chatId] = [...found_words[chatId], ...bingo_words];
-        console.log(found_words[chatId]);
-        if(found_words[chatId].length == 5){
-            io.emit(chatId, {name: "Admin", text: `BINGO! You win!! Please give feedback to improve the learning game.`, from: 'admin'});    
-        }
-        else{
-            const difference =  words.filter(x => !found_words[chatId].includes(x));
-            // we would want to use a color to show newly found words
-            io.emit(chatId, {name: "Admin", text: `You found the words:/你找到了这些词: ${found_words[chatId].join(", ")}, still missing: ${difference.join(", ")}`, from: 'admin'});    
-        }
-    }
+});*/
+let ws;
 
-});  
-  
+const wss = new WebSocket.WebSocketServer({ port: 8080 });
+
+console.log("started websocket server on port 8080");
+wss.on('connection', (socket) => {
+    ws = socket;
+    console.log("AI connected")
+
+    ws.on('open', () => {
+        ws.send(JSON.stringify({"text":'hi'}));
+        ws.send(JSON.stringify({"text":'begin'}));
+    });
+
+    ws.on('message', function message(data) {
+        const parsedData = JSON.parse(data)
+        console.log('received: %s', parsedData);
+        console.log(global_user_id);
+        console.log(global_chat_id);
+        const text = parsedData["text"];
+        console.log(text)
+        io.to(global_user_id).emit(global_chat_id + "-" + global_user_id, {from: "admin", text, name: "AI"});
+        const words = ["apple", "tree", "Eiffel", "Germany", "excellent"]
+        const chatId = global_chat_id;
+        if(words.some(el => text.includes(el))){
+            console.log("bingo word "+text)
+            const bingo_words = words.filter(el => text.includes(el));
+            console.log(bingo_words);
+            if(!found_words[chatId]){
+                found_words[chatId] = [];
+            }
+            found_words[chatId] = [...found_words[chatId], ...bingo_words];
+            console.log(found_words[chatId]);
+            if(found_words[chatId].length == 5){
+                io.emit(chatId, {name: "Admin", text: `BINGO! You win!! Please give feedback to improve the learning game.`, from: 'admin'});    
+            }
+            else{
+                const difference =  words.filter(x => !found_words[chatId].includes(x));
+                // we would want to use a color to show newly found words
+                io.emit(chatId, {name: "Admin", text: `You found the words:/你找到了这些词: ${found_words[chatId].join(", ")}, still missing: ${difference.join(", ")}`, from: 'admin'});    
+            }
+        }
+
+    });  
+    
+});
+
 app.use(express.static('dist', {index: 'demo.html', maxage: '4h'}));
 app.use(bodyParser.json());
 
@@ -87,9 +99,9 @@ app.post('/hook', function(req, res){
 });
 
 // handle chat visitors websocket messages
-io.on('connection', function(socket){
+io.on('connection', (socket) => {
 
-    socket.on('register', function(registerMsg){
+    socket.on('register', (registerMsg) => {
         let userId = registerMsg.userId;
         let chatId = registerMsg.chatId;
         global_user_id = userId;
@@ -135,19 +147,19 @@ function sendTelegramMessage(chatId, text, parseMode) {
         });
 }
 
-app.post('/usage-start', cors(), function(req, res) {
+app.post('/usage-start', cors(), (req, res) => {
     console.log('usage from', req.query.host);
     res.statusCode = 200;
     res.end();
 });
 
 // left here until the cache expires
-app.post('/usage-end', cors(), function(req, res) {
+app.post('/usage-end', cors(), (req, res) => {
     res.statusCode = 200;
     res.end();
 });
 
-http.listen(process.env.PORT || 3000, function(){
+http.listen(process.env.PORT || 3000, () => {
     console.log('listening on port:' + (process.env.PORT || 3000));
 });
 
